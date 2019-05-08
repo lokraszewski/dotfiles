db48aea108a15c39473339d60a23d088508f3e30
Lukasz Okraszewski <neo1936@gmail.com>
2019-05-08 20:01:08 +0100

    Added tmux conf

---
+++ install.sh
@@ -0,0 +1,90 @@
+# bin/bash
+
+prompt_install() {
+    echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
+    old_stty_cfg=$(stty -g)
+    stty raw -echo
+    answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+    stty $old_stty_cfg && echo
+    if echo "$answer" | grep -iq "^y" ;then
+        # This could def use community support
+        if [ -x "$(command -v apt-get)" ]; then
+            sudo apt-get install $1 -y
+
+        elif [ -x "$(command -v brew)" ]; then
+            brew install $1
+
+        elif [ -x "$(command -v pkg)" ]; then
+            sudo pkg install $1
+
+        elif [ -x "$(command -v pacman)" ]; then
+            sudo pacman -S $1
+
+        else
+            echo "No valid package manager found!"
+        fi
+    fi
+}
+
+check_if_installed() {
+    if ! [ -x "$(command -v $1)" ]; then
+        prompt_install $1
+    else
+        echo "$1 is installed."
+    fi
+}
+
+
+check_default_shell() {
+    if [ -z "${SHELL##*bash*}" ] ;then
+            echo "Default shell is bash."
+    else
+        echo -n "Default shell is not bash. Do you want to chsh -s \$(which bash)? (y/n)"
+        old_stty_cfg=$(stty -g)
+        stty raw -echo
+        answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+        stty $old_stty_cfg && echo
+        if echo "$answer" | grep -iq "^y" ;then
+            chsh -s $(which bash)
+        else
+            echo "Warning: Your configuration won't work properly. If you exec bash, it'll exec tmux which will exec your default shell which isn't bash."
+        fi
+    fi
+}
+
+git pull origin master
+
+check_if_installed vim
+echo
+
+check_if_installed tmux
+echo
+
+check_default_shell
+echo
+
+echo -n "Would you like to backup your current dotfiles? (y/n) "
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+if echo "$answer" | grep -iq "^y" ;then
+    mv ~/.tmux.conf ~/.tmux.conf.old
+    mv ~/.vimrc ~/.vimrc.old
+else
+    echo -e "\nNot backing up old dotfiles."
+fi
+
+
+rsync   --exclude ".git/" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        -avh --no-perms . ~;
+
+source ~/.bash_profile
+
+# cp tmux/.tmux.conf ~/
+# cp tmux/.tmux.conf.local ~/
+
+# cp vim/.vim.rc ~/.vimrc
+# cp vim/.vim.rc ~/.vimrc
+# cp vim/.vim.rc ~/.vimrc
+
+# echo "Please log out and log back in for default shell to be initialized."
