e07cc476642f8af91efa8141b49e7b78b049b31f
Lukasz Okraszewski <lukokr01@e121508-lin.manchester.arm.com>
2019-10-31 14:19:45 +0000

    packages + pip

--- install.sh
+++ install.sh
@@ -77,6 +77,37 @@ install_dotfiles () {
   done
 }
 
+install() {
+    if command_exists apt-get ; then
+        sudo apt-get install $1 -y
+    elif command_exists brew ; then
+        brew install $1
+    elif command_exists pkg ; then
+        sudo pkg install $1
+    elif command_exists pacman ; then
+        sudo pacman -S $1
+    else
+        error "No valid package manager found!"
+    fi
+}
+
+check_if_installed() {
+    if command_exists $1 ; then
+        success "$1 is installed."
+    else
+        install $1
+    fi
+}
+
+
+install_package(){
+  PACKAGE_FILE=$(find . -name "package_list")
+  while read p; do
+      info "Checking $p"
+      check_if_installed $p
+  done < $PACKAGE_FILE
+}
+
 run_all_install () {
   for installer in $(find . -name '*install*' -not -path './install.sh'); do
       info "running ${installer}"
@@ -88,6 +119,7 @@ run_all_install () {
 main (){
   echo ''
   install_dotfiles
+  install_package
   run_all_install
   success '  All installed!'
 }
