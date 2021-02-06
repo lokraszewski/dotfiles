6dcda5c718a854b3b00bb003bce438184874d044
Lukasz Okraszewski <neo1936@gmail.com>
2021-02-06 16:01:13 +0000

    upd: Switch to p10k as the theme

--- zsh/.zsh.d/zplug.zsh
+++ zsh/.zsh.d/zplug.zsh
@@ -6,6 +6,10 @@
 #  ╚══════╝╚══════╝╚═╝  ╚═╝
 # zplug
 
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 if [[ -z $ZPLUG_HOME ]]; then
     export ZPLUG_HOME=~/.zplug
 fi
@@ -27,10 +31,14 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "mafredri/zsh-async", from:"github"
-zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+# zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "romkatv/powerlevel10k", as:theme, depth:1
 
 if ! zplug check; then
     zplug install
 fi
 
 zplug load
+
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 #  ███████╗███████╗██╗  ██╗██████╗  ██████╗
 #  ╚══███╔╝██╔════╝██║  ██║██╔══██╗██╔════╝
 #    ███╔╝ ███████╗███████║██████╔╝██║
@@ -38,3 +45,6 @@ done
 if [[ "$ZPROF" = true ]]; then
     zprof
 fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
