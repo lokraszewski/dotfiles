35cbede78b1763707468067a1891b1fd56c35a0f
Lukasz Okraszewski <work@lokraszewski.com>
2023-10-25 20:03:02 +0100

    feat: first

---
+++ .tmux/functions/ip_local.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+export LC_ALL=en_US.UTF-8
+
+main()
+{
+
+  if [ -f "$HOME/.hidemyass" ] ; then
+      echo '13.37.13.37'
+      return
+  fi
+
+  case $(uname -s) in
+    # Only Linux for now since I am lazy, also I don't own a Mac.
+    Linux)
+      # hostname is unreliable since we cannot make assumptions about the order of addresses
+      ip route get 1 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n1
+      ;;
+
+    *)
+      ;;
+  esac
+}
+
+main
+
